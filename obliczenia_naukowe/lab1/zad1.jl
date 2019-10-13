346758b68928aa6b17e1e5af30d295b4a9fa30cb
ProtossLuigi <protossluigi@LAPTOP-OLT83PUT.localdomain>
2019-10-13 13:03:04 +0200

    Committer: ProtossLuigi <protossluigi@LAPTOP-OLT83PUT.localdomain>
     On branch master
     Your branch is up to date with 'studia/master'.
    
     Changes to be committed:
    	modified:   zad1.jl

--- obliczenia_naukowe/lab1/zad1.jl
+++ obliczenia_naukowe/lab1/zad1.jl
@@ -38,16 +38,48 @@ println("eta dla " * string(typeof(eta)) * " " * string(eta))
 println("nextfloat(Float64(0.0)) = " * string(nextfloat(Float64(0.0))))
 println("floatmin(Float32) = " * string(floatmin(Float32)))
 println("floatmin(Float64) = " * string(floatmin(Float64)))
+
+x = Float16(1.)
+while !isinf(x*2)
+        global x *= 2
+end
+i = Float16(1.)
+while !isinf(x+i)
+        if x + i == x
+                global i *= 2
+        else
+                global x += i
+        end
+end
+println(x)
+println(floatmax(Float16))
+
 x = 1f0
 while !isinf(x*2)
 	global x *= 2
 end
-i = 1f0
-while !isinf(x+i)
-	if x + i == x
-		global i *= 2
+i = x
+while !(x + i == x)
+	if isinf(x + i)
+		global i /= 2
 	else
 		global x += i
 	end
 end
 println(x)
+println(floatmax(Float32))
+
+x = 1e0
+while !isinf(x*2)
+        global x *= 2
+end
+i = x
+while x + i != x
+        if isinf(x + i)
+                global i /= 2
+        else
+                global x += i
+        end
+end
+println(x)
+println(floatmax(Float64))
