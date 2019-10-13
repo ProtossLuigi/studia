fd51861df2c1b97f4be7ab8ed0d58f9f3ee56926
ProtossLuigi <protossluigi@LAPTOP-OLT83PUT.localdomain>
2019-10-13 12:54:21 +0200

    Committer: ProtossLuigi <protossluigi@LAPTOP-OLT83PUT.localdomain>
     On branch master
     Your branch is ahead of 'studia/master' by 4 commits.
     (use "git push" to publish your local commits)
    
     Changes to be committed:
    	new file:   zad1.jl
    
     Changes not staged for commit:
    	deleted:    ../test.jl
    	modified:   ../../www/lab1/info.html
    	modified:   ../../www/lab1/interests.html
    	modified:   ../../www/lab1/projects.html
    	modified:   ../../www/lab1/style.css
    
     Untracked files:
    	../../www/lab1/.idea/

---
+++ obliczenia_naukowe/lab1/zad1.jl
@@ -0,0 +1,53 @@
+macheps = Float16(1.)
+while Float16(1.) + macheps / 2 > Float16(1.)
+	global macheps /= 2
+end
+println("macheps dla " * string(typeof(macheps)) * " " * string(macheps))
+
+macheps = Float32(1.)
+while Float32(1.) + macheps / 2 > Float32(1.)
+        global macheps /= 2
+end
+println("macheps dla " * string(typeof(macheps)) * " " * string(macheps))
+
+macheps = Float64(1.)
+while Float64(1.) + macheps / 2 > Float64(1.)
+        global macheps /= 2
+end
+println("macheps dla " * string(typeof(macheps)) * " " * string(macheps))
+
+eta = Float16(1.)
+while eta / 2 > Float16(0.0)
+	global eta /= 2
+end
+println("eta dla " * string(typeof(eta)) * " " * string(eta))
+println("nextfloat(Float16(0.0)) = " * string(nextfloat(Float16(0.0))))
+
+eta = Float32(1.)
+while eta / 2 > Float32(0.0)
+        global eta /= 2
+end
+println("eta dla " * string(typeof(eta)) * " " * string(eta))
+println("nextfloat(Float32(0.0)) = " * string(nextfloat(Float32(0.0))))
+
+eta = Float64(1.)
+while eta / 2 > Float64(0.0)
+        global eta /= 2
+end
+println("eta dla " * string(typeof(eta)) * " " * string(eta))
+println("nextfloat(Float64(0.0)) = " * string(nextfloat(Float64(0.0))))
+println("floatmin(Float32) = " * string(floatmin(Float32)))
+println("floatmin(Float64) = " * string(floatmin(Float64)))
+x = 1f0
+while !isinf(x*2)
+	global x *= 2
+end
+i = 1f0
+while !isinf(x+i)
+	if x + i == x
+		global i *= 2
+	else
+		global x += i
+	end
+end
+println(x)
