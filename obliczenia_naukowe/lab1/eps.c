ad75205480ba623ab2fbc6d3653faadc0d67bf0b
ProtossLuigi <protoss0@wp.pl>
2019-10-14 14:13:34 +0200

    vscode test

---
+++ obliczenia_naukowe/lab1/eps.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<float.h>
+int main(){
+printf("%f\n",FLT_EPSILON);
+printf("%lf\n",DBL_EPSILON);
+return 0;
+}
---
+++ obliczenia_naukowe/lab1/zad2.jl
@@ -0,0 +1,3 @@
+println(3*(Float16(4.)/3-1)-1)
+println(3*(4f0/3-1)-1)
+println(3*(4e0/3-1)-1)
