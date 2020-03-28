ae83fdd234a830079870f0d10da4b2bc3f258c00
ProtossLuigi <protoss0@wp.pl>
2020-03-28 19:20:56 +0100

    done python 3

--- python/lab3/zad4.py
+++ python/lab3/zad4.py
@@ -4,3 +4,10 @@ def qsort(to_sort):
     sorted1 = qsort([x for x in to_sort[1:] if x <= to_sort[0]])
     sorted2 = qsort([x for x in to_sort[1:] if x > to_sort[0]])
     return sorted1 + to_sort[:1] + sorted2
+
+# def qsort(to_sort):
+#     if not to_sort:
+#         return []
+#     sorted1 = qsort(list(filter(lambda x: x <= to_sort[0], to_sort[1:])))
+#     sorted2 = qsort(list(filter(lambda x: x > to_sort[0], to_sort[1:])))
+#     return sorted1 + to_sort[:1] + sorted2
--- python/lab3/zad5.py
+++ python/lab3/zad5.py
@@ -1,2 +1,8 @@
 def all_subsets(lst):
-    return [[] for j in range(0, 2**len(lst))]
+    return [[lst[i] for i in range(0, len(lst)) if 1 << i & j] for j in range(0, 2**len(lst))]
+
+# def all_subsets(lst):
+#     if not lst:
+#         return [[]]
+#     pre_lst = all_subsets(lst[:-1])
+#     return pre_lst + list(map(lambda x: x + [lst[-1]], pre_lst))
